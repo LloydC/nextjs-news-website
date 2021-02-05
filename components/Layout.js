ae5296c1647052eac6d913d75b335edfb8a3e6c9
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-02-05 16:44:26 +0000

    Added Header component to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
+import Header from '../components/Header'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
+                <Header />
                 {children}
             </main>
         </div>
